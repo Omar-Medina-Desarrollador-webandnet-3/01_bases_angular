e0560a9cf6ec188f0ac8ae6783a68ec75c38ec33
Omar Medina <desarrolladorwebandnet3@gmail.com>
2023-08-09 14:51:23 -0500

    Creación de componente Heroe Manualmente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -7,7 +7,44 @@ import { Component } from "@angular/core";
 })
 
 export class HeroeComponent {
+    title: string = 'SUPERMAN Y SUS PELICULAS';
+    nombre: string = 'SUPER MAN';
+    apellido: string = 'PARKERT';
+    edad: number = 35;
+    peliculas = Peliculas;
 
-    constructor() {
+    get peliculaCapitalizada():string {
+        return this.peliculas.segunda.toUpperCase();
+    }
+
+    //Función dentro de clase
+    obtenerNombre():string {
+        this.peliculaCapitalizada;
+        return `${this.nombre} - ${this.edad}`;
+        // return this.nombre + ' - ' + this.edad; vasicamente lo mismo
+        // return this.nombre + ' - ' + this.edad.toString(); vasicamente lo mismo
+    }
+
+    cambiarNombre():void {
+        this.nombre = 'Spiderman'.toUpperCase();
+    }
+
+    cambiarEdad():void{
+        this.edad = 67;
     }
 }
+
+const Peliculas: Pelicula = {
+    primera: 'El Hombre de Acero',
+    segunda: 'La liga de la Justicia',
+    tercera: 'Superman Regresa',
+    cuarta:  'The Death of Superman'
+}
+
+interface Pelicula {
+    primera: string;
+    segunda: string;
+    tercera: string;
+    cuarta:  string;
+}
+
